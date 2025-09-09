f1dc0f49840ec8bfa63508d45c1cebd7cb837b93
Faruk <farukderm@gmail.com>
2025-09-09 23:26:32 +0300

    test0

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const App = () => {
-  return <div className="text-blue-500">App</div>;
+  return (
+    <div className="text-blue-500">
+      <h1>Gl</h1>
+    </div>
+  );
 };
 
 export default App;
